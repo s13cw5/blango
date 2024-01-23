e992d05f06f5acb06166bbc7ce65a3f3c7bc562e
crosswire crosswire <xcell009@yahoo.com>
2024-01-23 22:00:24 +0000

    Finish custom template tags

--- blango/urls.py
+++ blango/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import blog.views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("post/<slug>/", blog.views.post_detail, name="blog-post-detail")
 ]
