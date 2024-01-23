9515dafbd29b17a5c769ab778a12ca588e5aed23
crosswire crosswire <xcell009@yahoo.com>
2024-01-23 21:11:20 +0000

    Finish html frameworks

--- blango/settings.py
+++ blango/settings.py
@@ -68,7 +68,7 @@ class Dev(Configuration):
     TEMPLATES = [
         {
             'BACKEND': 'django.template.backends.django.DjangoTemplates',
-            'DIRS': [],
+            'DIRS': [BASE_DIR / 'templates'],
             'APP_DIRS': True,
             'OPTIONS': {
                 'context_processors': [
