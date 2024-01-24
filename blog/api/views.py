ef1f1cf175b0118bcfa7be89ba5948a6c9b6a6af
crosswire crosswire <xcell009@yahoo.com>
2024-01-24 18:34:28 +0000

    Finish permissions

--- blog/api/views.py
+++ blog/api/views.py
@@ -2,11 +2,13 @@ from rest_framework import generics
 
 from blog.api.serializers import PostSerializer
 from blog.models import Post
+from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
 
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
